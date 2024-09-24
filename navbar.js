49b955df52ed934732413cb531a59a0d7911f9c2
SainteNick <108036685+SainteNick@users.noreply.github.com>
2024-09-24 14:16:03 -0300

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -22,20 +22,7 @@ const Navbar = () => {
                 </a>
               </div>
             </div>
-            <div className='hidden md:block'>
-              <div className='ml-4 flex items-center space-x-4'>
-                <a href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Inicio
-                </a>
-                <a href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Sobre nosotros
-                </a>
-                <a href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Contáctanos
-                </a>
-              </div>
-            </div>
-            <div className='md:hidden flex items-center'>
+            <div className='max-lg:hidden flex items-center'>
               <button className='inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
                 onClick={toggleNavbar}>
                 {isClick ? (
@@ -72,15 +59,15 @@ const Navbar = () => {
           </div>
         </div>
         {isClick && (
-            <div className='md:hidden'>
+            <div className='max-lg:hidden'>
                 <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                    <a href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
+                    <a href='/' className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>
                       Inicio
                     </a>
-                    <a href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
+                    <a href='/' className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>
                       Sobre nosotros
                     </a>
-                    <a href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
+                    <a href='/' className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>
                       Contáctanos
                     </a>   
                 </div>
