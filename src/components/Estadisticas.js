fe80d7ce7da590156f530e75b86484a7ad8e3877
AbdielAntonioMolinaCifuentes <135261424+AbdielAntonioMolinaCifuentes@users.noreply.github.com>
2024-10-23 00:38:58 -0300

    Logica Calculo Estadisticas con Datos Simulados

--- src/components/Estadisticas.js
+++ src/components/Estadisticas.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Estadisticas = () => {
+const Estadisticas = ({ datos }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-4">
-      <div className="mb-4">
+      <h3 className="text-gray-700 font-bold mb-2">ESTADÍSTICAS</h3>
+      <div>
         <label className="block text-gray-700 font-bold mb-2" htmlFor="promedio">
           PROMEDIO
         </label>
@@ -11,11 +12,12 @@ const Estadisticas = () => {
           id="promedio"
           type="text"
           className="border border-gray-300 px-2 py-1 w-full rounded-lg shadow-sm"
+          value={datos.promedio ? datos.promedio.toFixed(2) : "N/A"}
           readOnly
         />
       </div>
 
-      <div className="mb-4">
+      <div>
         <label className="block text-gray-700 font-bold mb-2" htmlFor="maximo">
           MÁXIMO
         </label>
@@ -23,11 +25,12 @@ const Estadisticas = () => {
           id="maximo"
           type="text"
           className="border border-gray-300 px-2 py-1 w-full rounded-lg shadow-sm"
+          value={datos.maximo !== undefined ? datos.maximo : "N/A"}
           readOnly
         />
       </div>
 
-      <div className="mb-4">
+      <div>
         <label className="block text-gray-700 font-bold mb-2" htmlFor="minimo">
           MÍNIMO
         </label>
@@ -35,6 +38,7 @@ const Estadisticas = () => {
           id="minimo"
           type="text"
           className="border border-gray-300 px-2 py-1 w-full rounded-lg shadow-sm"
+          value={datos.minimo !== undefined ? datos.minimo : "N/A"}
           readOnly
         />
       </div>
