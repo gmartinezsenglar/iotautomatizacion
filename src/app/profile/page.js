abf113066d53ee304d5dd9cea2534452d2b6fca8
SainteNick <nicolasfarri@gmail.com>
2024-11-19 13:41:16 -0300

    Actualizacion sprint3
    -cambio de estilo Footer
    -creacion de componente sidebar para pagina de informacion del usuario
    -cambios en los componentes de contenido del usuario y pagina del perfil

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,8 @@
 import { getSession } from "@/actions";
-import { redirect } from "next/navigation";
 import AdminSection from "@/components/AdminSection";
+import Sidebar from "@/components/Sidebar";
 import UserSection from "@/components/UserSection";
+import { redirect } from "next/navigation";
 
 export default async function Profile() {
   const session = await getSession();
@@ -15,8 +16,13 @@ export default async function Profile() {
   }
 
   return (
-    <div>
-        {session.rol === 'admin' ? <AdminSection /> : <UserSection />}
+    <div className="flex flex-col lg:flex-row justify-center items-start lg:space-x-6 p-6">
+      <div className="lg:w-64 w-full max-w-xs mb-6 lg:mb-0">
+        <Sidebar />
+      </div>
+      <div className="flex-1 bg-gray-50 rounded-lg shadow-md p-6">
+        {session.rol === "admin" ? <AdminSection /> : <UserSection />}
+      </div>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,61 @@
 "use client";
 
 import Link from "next/link";
-import React, { Component } from "react";
-
+import { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        const current_year = new Date().getFullYear();
-        return (
-            <div id="section_footer">
-                <div className="text-center py-4">
-                    <Link href="./contact" target="_blank" className="btn btn-link">
-                        Contacto
-                    </Link>
-                    <span>|</span>
-                    <Link href="../privacyPolicy/page.js" target="_blank" className="btn btn-link">
-                        Politica de Privacidad
-                    </Link>
-                    <span>|</span>
-                    <Link href="../app/termsConditions/page.js" target="_blank" className="btn btn-link">
-                        Terminos y condiciones
-                    </Link>
-                    <span>|</span>
-                </div>
-                <p className="text-center py-4">Copyright © {current_year}, IOT.sistemas.automatizados LLC. All Rights Reserved.</p>
-            </div>
-        );
-    }
+  render() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+      <footer className="bg-blue-600 text-white shadow m-0">
+        <div className="w-full p-4 md:py-8">
+          <div className="flex flex-wrap items-center justify-center sm:justify-between">
+            <Link
+              href="/"
+              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            >
+              <img src="/images/logo21.png" className="h-8" alt="Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">
+                IOT Automatizacion de Sistemas
+              </span>
+            </Link>
+            <ul className="flex flex-wrap items-center justify-center text-center sm:justify-start sm:text-left mb-6 text-sm font-medium text-white">
+              <li className="w-full md:w-auto md:me-6 mb-2 md:mb-0">
+                <Link href="./contact" className="hover:underline">
+                  Contacto
+                </Link>
+              </li>
+              <li className="w-full md:w-auto md:me-6 mb-2 md:mb-0">
+                <Link
+                  href="../privacyPolicy/page.js"
+                  className="hover:underline"
+                >
+                  Política de Privacidad
+                </Link>
+              </li>
+              <li className="w-full md:w-auto">
+                <Link
+                  href="../app/termsConditions/page.js"
+                  className="hover:underline"
+                >
+                  Términos y Condiciones
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <hr className="my-6 border-gray-200 lg:my-8" />
+          <span className="block text-sm text-center text-white">
+            © {currentYear}{" "}
+            <Link href="/" className="hover:underline">
+              IOT Sistemas Automatizados™
+            </Link>
+            . Todos los derechos reservados.
+          </span>
+        </div>
+      </footer>
+    );
+  }
 }
+
 export default Footer;
